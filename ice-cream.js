b50d370967d2268a3bbd51506b5201f48186d275
Tom Zhang <tom.zhang989@gmail.com>
2021-08-18 17:13:14 -0400

    this is the icecream array teaching array dec and basic indexing;

---
+++ ice-cream.js
@@ -0,0 +1,12 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+//My code
+iceCreamFlavours.push('root beer')
+
+console.log(iceCreamFlavours)
+console.log(iceCreamFlavours[0])
+
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1])
+
+console.log(iceCreamFlavours.length)
