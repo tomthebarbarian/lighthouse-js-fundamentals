6a59ac6bfa7b0cb0079c68a2cd6d83fe38ebdb41
Tom Zhang <tom.zhang989@gmail.com>
2021-08-21 19:04:28 -0400

    this camel case maker works with a regular whitespace string

---
+++ casemake.js
@@ -0,0 +1,32 @@
+// function camelCase takes a string(input) and returns a string that 
+// has input's white space stripped and is in camelCase.
+
+// str -> str 
+
+const camelCase = function(input) {
+  // Your code here
+  let toupper = false
+  let ansstr = ''
+  for (let elem of input){
+    //console.log(elem)
+    if (elem === ' '){
+      toupper = true
+    } else if (toupper === true) {
+      if (elem !== ' '){
+        ansstr += elem.toUpperCase()
+        toupper = false
+      }
+    } else {
+      ansstr += elem
+    }
+    
+  }
+  return ansstr
+};
+
+//Test Cases
+/*
+console.log(camelCase("this is a string"));
+console.log(camelCase("loopy lighthouse"));
+console.log(camelCase("supercalifragalisticexpialidocious"));
+*/
