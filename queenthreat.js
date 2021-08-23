a23a296d8e4586cd5a1c851dcf06c62662852e29
Tom Zhang <tom.zhang989@gmail.com>
2021-08-23 15:16:48 -0400

    changed code so that it actually goes inside the array of arrays now

--- queenthreat.js
+++ queenthreat.js
@@ -1,5 +1,9 @@
 
 
+// function generateBoard takes in 2 arraysOf nums(whiteQueen, blackQueen) 
+// with length 2 and returns an arrayOf arraysOf num representing a chessboard
+
+// arrayOf num arrayOf num -> arrayOf arraysOf num
 const generateBoard = (whiteQueen,blackQueen) => {
   let ansboard = [
     [0, 0, 0, 0, 0, 0, 0, 0],
@@ -16,13 +20,18 @@ const generateBoard = (whiteQueen,blackQueen) => {
   return ansboard
 }
 
+
+// function queenThreat takes an arrayOf arraysOf num(generated board) representing
+// the position of 2 queens and returns a boolean indicating if 2 queen pieces
+// threaten each other. 
 const queenThreat = (generatedBoard) =>{
   let queen1 = []
   let queen2 = []
-
-  for (let elem = 0; elem < generatedBoard.length; elem ++){
-    for (let space = 0; space < elem.length; space ++){
-      if (elem[space] === 1){
+  for (let elem = 0; elem < generatedBoard.length; elem++){
+    let currow = generatedBoard[elem]
+    for (let space = 0; space < currow.length; space++){
+      //console.log(currow[space])
+      if (currow[space] === 1){
         if (queen1.length > 1){
           queen2 = [elem, space]
           break
@@ -36,6 +45,8 @@ const queenThreat = (generatedBoard) =>{
       break
     }
   }
+  console.log(queen1)
+  console.log(queen2)
   if (queen1[0] === queen2[0] || queen1[1] === queen2[1]){
     return true
   } else if ((queen1[0] - queen2[0]) **2 === (queen1[1] - queen2[1])**2){
@@ -46,8 +57,8 @@ const queenThreat = (generatedBoard) =>{
 
 }
 
-let whiteQueen = [0, 0];
-let blackQueen = [5, 7];
+let whiteQueen = [0, 4];
+let blackQueen = [5, 0];
 let generatedBoard = generateBoard(whiteQueen, blackQueen);
-console.log(generatedBoard);
+//console.log(generatedBoard);
 console.log(queenThreat(generatedBoard));
