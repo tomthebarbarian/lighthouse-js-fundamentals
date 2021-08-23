fed1140a802af4770ed6376501af2b298b7700a0
Tom Zhang <tom.zhang989@gmail.com>
2021-08-23 14:04:04 -0400

    first submitted inflate vol

--- inflatevol.js
+++ inflatevol.js
@@ -3,27 +3,46 @@ const PI = 3.14159 ;
 
 const sphereVolume = function (radius) {
   // Code here! 
-  (4 / 3) * (radius ** 3) * PI
+  return (4 / 3) * (radius ** 3) * PI
 }
+
 
 console.log(4186 < sphereVolume(10) && sphereVolume(10) < 4189);
 
 const coneVolume = function (radius, height) {
   // And here!
-  (height / 3) * radius ** 3
+  return (height / 3) * (radius ** 2) * PI
 }
-}
+
 
 console.log(45 < coneVolume(3, 5) && coneVolume(3, 5) < 49);
 
 const prismVolume = function (height, width, depth) {
   // Probably here too!
+  return height * width * depth
 }
 
 console.log(prismVolume(3, 4, 5) === 60);
 
 const totalVolume = function (solids) {
   // Code here? Yup!
+  let total = 0
+  for (let elem of solids){
+    console.log(elem.type)
+    switch (elem.type){
+    case 'sphere':
+      total += sphereVolume(elem.radius);
+      continue;
+    case 'cone':
+      console.log('incone')
+      total += coneVolume(elem.radius, elem.height);
+      continue;
+    case 'prism':
+      total += prismVolume(elem.height, elem.width, elem.depth);
+      
+    }
+  }
+  return total
 }
 
 const largeSphere = {
@@ -47,5 +66,6 @@ const duck = [
   smallSphere,
   cone
 ]
+
 
 console.log(272000 < totalVolume(duck) && totalVolume(duck) < 275000);
