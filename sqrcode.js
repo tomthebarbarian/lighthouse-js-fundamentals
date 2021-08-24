c08843b51957b74ff77846cb544dee79318e6395
Tom Zhang <tom.zhang989@gmail.com>
2021-08-24 15:29:52 -0400

    commented out the tests

--- sqrcode.js
+++ sqrcode.js
@@ -45,7 +45,7 @@ const squareCode = function(message) {
 };
 
 //console.log(squareCode("chill out"));
-console.log(squareCode("feed the dog"));
+//console.log(squareCode("feed the dog"));
 //console.log(squareCode("have a nice day"));
 //console.log(squareCode("if man was meant to stay on the ground god would have given us roots"));
 
