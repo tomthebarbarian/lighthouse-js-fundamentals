b105b6d4eee1a6f4f4710c4106a98424f009ec1c
Tom Zhang <tom.zhang989@gmail.com>
2021-08-24 11:36:21 -0400

    fleshed out the actions taken for each case and improved the function description.

--- caseconvertadv.js
+++ caseconvertadv.js
@@ -1,4 +1,6 @@
-//
+// function makeCase takes an input string (input) and a casestyle or an 
+// array thereof and returns the input string modified to fulfill the 
+// specificiations of the casestyle(s)
 
 const makeCase = function(input, casetype) {
   // Put your solution here
@@ -33,15 +35,68 @@ const makeCase = function(input, casetype) {
           }
         }
       case 'pascal':
+        for (let chars of input){
+          if (ansstr === ''){
+            ansstr += chars.toUpperCase()
+          } else if (postwhite && chars !== ' '){
+            ansstr += chars.toUpperCase()
+            postwhite = false
+          } else if ( chars === ' '){
+            postwhite = true
+          } else {
+            ansstr += chars
+          }
+        }
       case 'snake':
+        for (let chars of input){
+          if ( chars === ' '){
+            ansstr += '_'
+          } else {
+            ansstr += chars
+          }
+        }
       case 'kebab':
+        for (let chars of input){
+          if ( chars === ' '){
+            ansstr += '-'
+          } else {
+            ansstr += chars
+          }
+        }
       case 'title':
+        for (let chars of input){
+          if (ansstr === ''){
+            ansstr += chars.toUpperCase()
+          } else if (postwhite && chars !== ' '){
+            ansstr += chars.toUpperCase()
+            postwhite = false
+          } else if ( chars === ' '){
+            ansstr += chars
+            postwhite = true
+          } else {
+            ansstr += chars
+          }
+        }
       case 'vowel':
-      case 'camel':
+        for (let chars of input){
+          if (vowels.includes(chars)){
+            ansstr += chars.toUpperCase()
+          } else {
+            ansstr += chars
+          }
+        }
       case 'consonant':
+        for (let chars of input){
+          if (!vowels.includes(chars)){
+            ansstr += chars.toUpperCase()
+          } else {
+            ansstr += chars
+          }
+        }
       case 'upper':
+        ansstr = ansstr.toUpperCase()
       case 'lower':
-                  
+        ansstr = ansstr.toLowerCase()
       }
     }
   }
