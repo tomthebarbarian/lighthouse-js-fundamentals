4631b32a33ddd0427a0b7c814bea001a931ad392
Tom Zhang <tom.zhang989@gmail.com>
2021-08-24 13:03:12 -0400

    initialization and description of the square code function.

---
+++ sqrcode.js
@@ -0,0 +1,11 @@
+// function squareCode takes an input string(message) and outputs a 
+// modified string that has its letters rearranged by using the square code method
+ 
+const squareCode = function(message) {
+  // Put your solution here
+};
+
+console.log(squareCode("chill out"));
+console.log(squareCode("feed the dog"));
+console.log(squareCode("have a nice day"));
+console.log(squareCode("if man was meant to stay on the ground god would have given us roots"));
