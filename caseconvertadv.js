045d98f4e063eb20518b7e9e0fd744532a5d9cde
Tom Zhang <tom.zhang989@gmail.com>
2021-08-24 12:27:53 -0400

    testing cases

--- caseconvertadv.js
+++ caseconvertadv.js
@@ -12,30 +12,31 @@ const makeCase = function(input, casetype) {
     //make single strs a len 1 array.
     casetype = [casetype]
   }
-  let prio1 = [camel, pascal, snake, kebab, title]
-  let prio2 = [vowel, consonant]
-  let prio3 = [upper, lower]
+  let prio1 = ['camel', 'pascal', 'snake', 'kebab', 'title']
+  let prio2 = ['vowel', 'consonant']
+  let prio3 = ['upper', 'lower']
   let ordofop = {
     stage1:[],
     stage2:[],
     stage3:[]
   }
   // Determine ordofop
-  if (casetype.length> 1){  
-    for (let elem of casetype){
-      if (prio1.includes(elem)){
-        ordofop.stage1 = ordofop.stage1.concat(elem)
-      } else if (prio2.includes(elem)){
-        ordofop.stage2 = ordofop.stage2.concat(elem)
-      } else if  (prio3.includes(elem)){
-        ordofop.stage3 = ordofop.stage3.concat(elem)
-      }
+  for (let elem of casetype){
+    console.log(elem)
+    if (prio1.includes(elem)){
+      ordofop.stage1 = ordofop.stage1.concat(elem)
+    } else if (prio2.includes(elem)){
+      ordofop.stage2 = ordofop.stage2.concat(elem)
+    } else if  (prio3.includes(elem)){
+      ordofop.stage3 = ordofop.stage3.concat(elem)
     }
-  } 
+  }
+  
 
   // Actually doing
   let firststage = ''
   //Prio 1
+  console.log(ordofop.stage1)
   for (let elem of ordofop.stage1){
     switch(elem){
     case 'camel':
@@ -49,6 +50,7 @@ const makeCase = function(input, casetype) {
           firststage += chars
         }
       }
+      break;
     case 'pascal':
       for (let chars of input){
         if (firststage === ''){
@@ -70,6 +72,7 @@ const makeCase = function(input, casetype) {
           firststage += chars
         }
       }
+      break;
     case 'kebab':
       for (let chars of input){
         if ( chars === ' '){
@@ -78,12 +81,13 @@ const makeCase = function(input, casetype) {
           firststage += chars
         }
       }
+      break;
     case 'title':
       for (let chars of input){
         if (firststage === ''){
           firststage += chars.toUpperCase()
         } else if (postwhite && chars !== ' '){
-          ansstr += chars.toUpperCase()
+          firststage += chars.toUpperCase()
           postwhite = false
         } else if ( chars === ' '){
           firststage += chars
@@ -92,13 +96,14 @@ const makeCase = function(input, casetype) {
           firststage += chars
         }
       }
+      break;
     }
   } 
 
     //prio 2
     let secondstage = ''
     if (firststage === '') {
-      secondstage = input
+      firststage = input
     }
     for (let elem of ordofop.stage2){
       switch(elem){
@@ -110,6 +115,7 @@ const makeCase = function(input, casetype) {
             secondstage += chars
           }
         }
+        break;
       case 'consonant':
         for (let chars of firststage){
           if (!vowels.includes(chars)){
@@ -119,9 +125,9 @@ const makeCase = function(input, casetype) {
           }
         }
       }
+      break;
     }
-    //prio3
-    let thirdstage = secondstage
+    //prio3 stage 3
     if (secondstage === '') {
       thirdstage = firststage || input
     }
@@ -129,17 +135,20 @@ const makeCase = function(input, casetype) {
       switch(elem){
       case 'upper':
         thirdstage = thirdstage.toUpperCase()
+        break;
       case 'lower':
         thirdstage = thirdstage.toLowerCase()
+        break;
       }
     }
+    return thirdstage
 }
 
 console.log(makeCase("this is a string", "camel"));
-console.log(makeCase("this is a string", "pascal"));
-console.log(makeCase("this is a string", "snake"));
-console.log(makeCase("this is a string", "kebab"));
-console.log(makeCase("this is a string", "title"));
-console.log(makeCase("this is a string", "vowel"));
-console.log(makeCase("this is a string", "consonant"));
-console.log(makeCase("this is a string", ["upper", "snake"]));
+//console.log(makeCase("this is a string", "pascal"));
+//console.log(makeCase("this is a string", "snake"));
+//console.log(makeCase("this is a string", "kebab"));
+//console.log(makeCase("this is a string", "title"));
+//console.log(makeCase("this is a string", "vowel"));
+//console.log(makeCase("this is a string", "consonant"));
+//console.log(makeCase("this is a string", ["upper", "snake"]));
